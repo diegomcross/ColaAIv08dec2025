6758b69d7b29464424f6c8a384cb5ef58c104558
diegomcross <diego.mcross@gmail.com>
2025-12-08 13:07:23 -0500

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 CHANNEL_RANKING = 1361499182304592012
 CHANNEL_MAIN_CHAT = 1357316018535403661
 CHANNEL_EVENT_LOGS = 1357722794674229351 # Canal para logs de conclusão
-CHANNEL_POLLS = 123456789012345678 # Substitua pelo ID do canal de enquetes
+CHANNEL_POLLS = 1447650420691570748 # Substitua pelo ID do canal de enquetes
 
 # Categoria onde os canais de eventos serão criados (ID Corrigido)
 CATEGORY_EVENTS_ID = 1357809570298331295
