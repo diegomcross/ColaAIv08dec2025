a19b980d69c99e8d83a047068a21a9b719d1cdab
diegomcross <diego.mcross@gmail.com>
2025-12-08 20:15:35 -0500

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 CHANNEL_RANKING = 1361499182304592012
 CHANNEL_MAIN_CHAT = 1357316018535403661
 CHANNEL_EVENT_LOGS = 1357722794674229351 
-CHANNEL_POLLS = 1447650420691570748 # Canal apenas para notificações automáticas/logs de enquetes
+CHANNEL_POLLS = 1447650420691570748 # Canal de Enquetes
 CHANNEL_LFG = 1435294107991019560   # Canal "Procure Atividades"
 
 # Categoria onde os canais de eventos serão criados
